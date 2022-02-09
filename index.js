dce4b09fe771bfddd0914fa4fc80e44bf652d4f5
Davisa94 <Austin.i.davis94@gmail.com>
2022-02-08 23:50:43 -0800

    Finished logic for the app

--- index.js
+++ index.js
@@ -6,6 +6,37 @@
  * The Server Version is another project for the future
  **************************************************/
 
+/**************************************************
+ * The Main logic function for determining if the 
+ * given number is fizz buzz or the number given
+ * Takes: integer
+ * Returns: String containing what it is designated
+ *          i.e fizz/buzz/#
+ *************************************************/
+function fizzBuzzLogic(number)
+{
+   determinedValue = "";
+   if(number % 3 == 0)
+   {
+      determinedValue = "Fizz"
+   }
+   if(number % 5 == 0)
+   {
+      determinedValue += "Buzz"
+   }
+   else
+   {
+      determinedValue = number;
+   }
+   return determinedValue;
+}
+
+/***************************************************
+ * The function that generates the 
+ */
+
+
+
 function main()
 {
    return 0;
