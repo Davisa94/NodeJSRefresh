f2b1bdeba7a5c41cbe7ae504d7a297a4ec822a12
Davisa94 <Austin.i.davis94@gmail.com>
2022-02-08 23:42:14 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/**************************************************
+ * A refresher on Node and Javascript
+ * Author: Austin Benitez 
+ * Date 2/8/2022
+ * This is a JS implimentation of FizzBuzz via CLI
+ * The Server Version is another project for the future
+ **************************************************/
